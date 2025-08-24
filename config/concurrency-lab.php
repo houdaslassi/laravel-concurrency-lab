9d95ee91c45e45e307260c8ffda4a72d16adae54
houdaslassi <houdaslassi.18@gmail.com>
2025-08-24 01:01:08 +0100

    feat: concurrency-lab config file

---
+++ config/concurrency-lab.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    // Funnel name & limit for parallel jobs
+    'funnel_key' => 'deployments',
+    'funnel_limit' => 5,
+
+    // How long a job should simulate work (seconds)
+    'sleep_seconds' => 5,
+];
